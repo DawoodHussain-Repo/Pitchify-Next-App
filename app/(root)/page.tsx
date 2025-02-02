f5efd01d1cf9ff3f88a5aa019b9b90672ce2df01
Dawood_Hussain <127896325+dawood90999@users.noreply.github.com>
2025-02-02 05:05:40 +0500

    Sanity Connected and Adjusted

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
+import { client } from "@/sanity/lib/client";
 import SearchForm from "../../components/SearchForm";
-
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 
 export default async function Home( { searchParams }:  {
   searchParams:Promise<{query?:string}>;
 }) {
+
+  const POSTS = await client.fetch(STARTUPS_QUERY);
   const query = (await searchParams).query ;
+console.log(POSTS);
   return (
     <div>
           <section className="pink_container">
@@ -20,6 +25,20 @@ export default async function Home( { searchParams }:  {
         </p>
 
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+<p className="text-30-semibold">
+  {query ? `Search results for "${query}"` : "Latest Pitches"}
+</p>
+<ul className="mt-7 card_grid">
+{POSTS.length > 0 ? (
+  POSTS.map((post:StartupTypeCard,index:number) => (
+   <StartupCard key={POSTS[index]._id} post={POSTS[index]}/>
+  ))
+):(
+  <h1 className="text-30-semibold">No Startup Listed Yet :(</h1>
+)}
+</ul>
       </section>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import 'easymde/dist/easymde.min.css';
 export const metadata: Metadata = {
   title: "Pitchify",
   description: "Pitchify is a platform for sharing Ideas.",
